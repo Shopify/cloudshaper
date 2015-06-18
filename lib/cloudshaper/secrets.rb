55e6d6fb3e513d5e48938aa9c44037da4e9f1272
Dale Hamel <dale.hamel@shopify.com>
2015-06-18 11:23:45 -0400

    Handle multiple secrets files, handle env secrets and variables separately

--- lib/cloudshaper/secrets.rb
+++ lib/cloudshaper/secrets.rb
@@ -1,29 +1,18 @@
 require 'json'
 require 'open3'
 
-# Load and provide access to secrets required by terraform providers
-class SecretHash < Hash
-  class SecretNotFound < Exception; end
+SECRETS ||= {}
 
-  def initialize
-    super { |_secrets, key| fail SecretNotFound, "Secret `#{key}` not found" }
-  end
-end
-
-if ENV['TERRAFORM_ENV'] == 'test'
-  SECRETS ||= {
-    aws: {
-      access_key_id: 'foo',
-      secret_access_key: 'bar'
-    }
-  }
-else
-  SECRETS ||= begin
-    secrets_file = File.expand_path(ENV['CONFIG_PATH'] || 'config/secrets.json')
-    if File.exist?(secrets_file)
-      JSON.parse(File.read(secrets_file), symbolize_names: true, object_class: SecretHash)
+secrets_files = (ENV['SECRETS_FILES'] || 'config/secrets.ejson').split(',')
+secrets_files.each do |secrets_file|
+  if File.exist?(secrets_file)
+    if secrets_file.end_with?('.ejson')
+      secrets = `ejson decrypt #{secrets_file}`
+    elsif secrets_file.end_with('.json')
+      secrets = File.read(secrets_file)
     else
-      {}
+      fail "I don't understand how to get secrets from #{secrets_file}"
     end
+    SECRETS.merge!(JSON.parse(secrets))
   end
 end
